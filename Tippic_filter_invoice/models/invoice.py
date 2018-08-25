140a069a1d1039cc68efb052e40d14c2335978fb
tippic <36774370+tippic@users.noreply.github.com>
2018-08-25 10:51:40 -0500

    Update invoice.py

--- Tippic_filter_invoice/models/invoice.py
+++ Tippic_filter_invoice/models/invoice.py
@@ -12,4 +12,4 @@ class AccountInvoice(models.Model):
             else:
                 invoice.days_due = False
     
-    days_due=fields.Integer(compute=_days_due)
+    Vencimiento=fields.Integer(compute=_days_due)
